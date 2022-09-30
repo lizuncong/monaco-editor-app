3bf6ab8e4f4f657c0f18f3da96a61683c0ba4ccf
lizuncong <2900170896@qq.com>
2022-09-30 14:36:58 +0800

    feat: stash

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -3,6 +3,9 @@ const baseConfig = require('./webpack.config');
 
 const prodConfig = {
     mode: 'production',
+    output: {
+        publicPath: 'https://lizuncong.github.io/monaco-editor-app',
+    },
 };
 const config = merge(baseConfig, prodConfig);
 module.exports = config;
